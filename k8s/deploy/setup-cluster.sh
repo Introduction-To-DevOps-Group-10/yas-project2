44faee1fdcc5e83234864632d4d504ae8e4b70eb
TUNAS106 <vantuanphuninh@gmail.com>
2026-04-16 09:37:35 +0700

    comment observability in setup-cluster

--- k8s/deploy/setup-cluster.sh
+++ k8s/deploy/setup-cluster.sh
@@ -6,9 +6,9 @@ helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-o
 helm repo add strimzi https://strimzi.io/charts/
 helm repo add akhq https://akhq.io/
 helm repo add elastic https://helm.elastic.co
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+# helm repo add grafana https://grafana.github.io/helm-charts
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
@@ -64,15 +64,17 @@ helm upgrade --install elasticsearch-cluster ./elasticsearch/elasticsearch-clust
   --set elasticsearch.replicas="$ELASTICSEARCH_REPLICAES" \
   --set kibana.ingress.hostname="kibana.$DOMAIN"
 
+# [OBSERVABILITY - DISABLED]
 # Install loki
-helm upgrade --install loki grafana/loki \
-  --create-namespace --namespace observability \
-  -f ./observability/loki.values.yaml
+# helm upgrade --install loki grafana/loki \
+#   --create-namespace --namespace observability \
+#   -f ./observability/loki.values.yaml
 
+# [OBSERVABILITY - DISABLED]
 # Install tempo
-helm upgrade --install tempo grafana/tempo \
-  --create-namespace --namespace observability \
-  -f ./observability/tempo.values.yaml
+# helm upgrade --install tempo grafana/tempo \
+#   --create-namespace --namespace observability \
+#   -f ./observability/tempo.values.yaml
 
 # Install cert manager
 helm upgrade --install cert-manager jetstack/cert-manager \
@@ -84,58 +86,60 @@ helm upgrade --install cert-manager jetstack/cert-manager \
   --set webhook.timeoutSeconds=4 \
   --set admissionWebhooks.certManager.create=true
 
-# FIX 1: Chờ cert-manager webhook sẵn sàng trước khi tiếp tục
+# Chờ cert-manager webhook sẵn sàng trước khi tiếp tục
 echo "Waiting for cert-manager to be ready..."
 kubectl wait --for=condition=Available deployment/cert-manager-webhook \
   --namespace cert-manager \
   --timeout=120s
 
+# [OBSERVABILITY - DISABLED]
 # Install opentelemetry-operator
-helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator \
-  --create-namespace --namespace observability
+# helm upgrade --install opentelemetry-operator open-telemetry/opentelemetry-operator \
+#   --create-namespace --namespace observability
 
-# FIX 2: Chờ opentelemetry-operator webhook sẵn sàng trước khi deploy collector
-echo "Waiting for opentelemetry-operator to be ready..."
-kubectl wait --for=condition=Available deployment/opentelemetry-operator \
-  --namespace observability \
-  --timeout=120s
+# [OBSERVABILITY - DISABLED]
+# Chờ opentelemetry-operator webhook sẵn sàng trước khi deploy collector
+# echo "Waiting for opentelemetry-operator to be ready..."
+# kubectl wait --for=condition=Available deployment/opentelemetry-operator \
+#   --namespace observability \
+#   --timeout=120s
 
+# [OBSERVABILITY - DISABLED]
 # Install opentelemetry-collector
-helm upgrade --install opentelemetry-collector ./observability/opentelemetry \
-  --create-namespace --namespace observability
+# helm upgrade --install opentelemetry-collector ./observability/opentelemetry \
+#   --create-namespace --namespace observability
 
+# [OBSERVABILITY - DISABLED]
 # Install promtail
-helm upgrade --install promtail grafana/promtail \
-  --create-namespace --namespace observability \
-  --values ./observability/promtail.values.yaml
+# helm upgrade --install promtail grafana/promtail \
+#   --create-namespace --namespace observability \
+#   --values ./observability/promtail.values.yaml
 
-# FIX 3: Không set database.password trực tiếp vào values file (bị Grafana chart chặn)
-# Thay vào đó chỉ set hostname và username vào values, password truyền qua --set-string lúc helm install
-grafana_hostname="grafana.$DOMAIN" yq -i '.hostname=env(grafana_hostname)' ./observability/prometheus.values.yaml
-postgresql_username="$POSTGRESQL_USERNAME" yq -i '.grafana."grafana.ini".database.user=env(postgresql_username)' ./observability/prometheus.values.yaml
+# [OBSERVABILITY - DISABLED]
+# Install prometheus + grafana stack
+# grafana_hostname="grafana.$DOMAIN" yq -i '.hostname=env(grafana_hostname)' ./observability/prometheus.values.yaml
+# postgresql_username="$POSTGRESQL_USERNAME" yq -i '.grafana."grafana.ini".database.user=env(postgresql_username)' ./observability/prometheus.values.yaml
+# helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+#   --create-namespace --namespace observability \
+#   -f ./observability/prometheus.values.yaml \
+#   --set grafana.assertNoLeakedSecrets=false \
+#   --set-string 'grafana.grafana\.ini.database.password'="$POSTGRESQL_PASSWORD"
 
-# Dùng --set-string để truyền password trực tiếp, không ghi vào file values
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
-  --create-namespace --namespace observability \
-  -f ./observability/prometheus.values.yaml \
-  --set grafana.assertNoLeakedSecrets=false \
-  --set-string 'grafana.grafana\.ini.database.password'="$POSTGRESQL_PASSWORD"
-  
-
+# [OBSERVABILITY - DISABLED]
 # Install grafana operator
-helm upgrade --install grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator \
-  --version v5.0.2 \
-  --create-namespace --namespace observability
+# helm upgrade --install grafana-operator oci://ghcr.io/grafana-operator/helm-charts/grafana-operator \
+#   --version v5.0.2 \
+#   --create-namespace --namespace observability
 
-# Add datasource and dashboard to grafana
-# FIX 4: Sửa typo "hotname" → "hostname"
-helm upgrade --install grafana ./observability/grafana \
-  --create-namespace --namespace observability \
-  --set hostname="grafana.$DOMAIN" \
-  --set grafana.username="$GRAFANA_USERNAME" \
-  --set grafana.password="$GRAFANA_PASSWORD" \
-  --set postgresql.username="$POSTGRESQL_USERNAME" \
-  --set postgresql.password="$POSTGRESQL_PASSWORD"
+# [OBSERVABILITY - DISABLED]
+# Install grafana datasource and dashboard
+# helm upgrade --install grafana ./observability/grafana \
+#   --create-namespace --namespace observability \
+#   --set hostname="grafana.$DOMAIN" \
+#   --set grafana.username="$GRAFANA_USERNAME" \
+#   --set grafana.password="$GRAFANA_PASSWORD" \
+#   --set postgresql.username="$POSTGRESQL_USERNAME" \
+#   --set postgresql.password="$POSTGRESQL_PASSWORD"
 
 # Install zookeeper
 helm upgrade --install zookeeper ./zookeeper \
