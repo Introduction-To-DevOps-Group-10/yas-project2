e6898734841feeae16a5dae7b9b7883c84d118ba
TUNAS106 <vantuanphuninh@gmail.com>
2026-04-18 00:12:06 +0700

    fix(search): lazy initialize elasticsearch repository

--- search/src/main/java/com/yas/search/repository/ProductRepository.java
+++ search/src/main/java/com/yas/search/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.yas.search.repository;
 
 import com.yas.search.model.Product;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Lazy
 public interface ProductRepository extends ElasticsearchRepository<Product, Long> {
 }
